d08f52a62f45f1f8f43cdeaab45b262df2d407c0
Andrew O'Connor <andrew.oconnor@gmail.com>
2017-11-05 08:29:15 +0000

    Removed log statements from interrupt handling

--- main.go
+++ main.go
@@ -31,10 +31,7 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	signal.Notify(c, syscall.SIGTERM)
-	log.Info("Waiting for interrupt")
 	<-c
-	log.Info("Received interrupt. Exiting.")
-	os.Exit(0)
 }
 
 func newHello(id string) step.F {
